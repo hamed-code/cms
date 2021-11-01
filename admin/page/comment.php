349edb4d7742cdc6a075d84e6776980cf58db3f3
morning coder <hamedmessi30@gmail.com>
2021-11-01 18:20:02 +0330

    make admin panel

---
+++ admin/page/comment.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/bootstrap.css">
+    <title>Admin</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <ul class="nav nav-pills nav-fill">
+                <li class="nav-item">
+                    <a class="nav-link" aria-current="page" href="../page/menu.php">منو</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../page/blog.php">وبلاگ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="#">کامنت</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+</body>
+
+<script src="../../js/jquery-3.5.1.min.js"></script>
+<script src="../../js/bootstrap.min.js"></script>
+
+</html>
